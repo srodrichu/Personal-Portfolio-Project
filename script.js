b729a52a3e767a12feb95e6f32717b60d588d22c
srodrichu <sebastianawrchu@gmail.com>
2021-08-21 18:54:48 +0100

    built sidebar with icons

---
+++ script.js
@@ -0,0 +1,8 @@
+function openForm() {
+        document.getElementById("form_popup").style.display = "block";
+        
+}
+
+function closeForm() {
+    document.getElementById("form_popup").style.display = "none"
+}
